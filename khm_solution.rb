1ac797033161af7bb3dbe087ac2ad9e05f4c2fba
Krystan Menne <kmenne@gmail.com>
2016-05-21 14:24:35 -0700

    retrieve COUNT_CALLS_TO environment variable

--- khm_solution.rb
+++ khm_solution.rb
@@ -2,4 +2,8 @@ class KHMSolution
   def self.hi
     puts 'Hello world!'
   end
+
+  def self.target_method
+    ENV['COUNT_CALLS_TO']
+  end
 end
--- spec/khm_solution_spec.rb
+++ spec/khm_solution_spec.rb
@@ -2,11 +2,30 @@ require 'rspec'
 require './khm_solution'
 
 describe KHMSolution do
+  let(:output_text_file) { 'tmp/test_output.txt' }
+
+  before :each do
+    File.delete(output_text_file) if File.exist?(output_text_file)
+  end
+
   describe '#hi' do
     it 'puts Hello World on the command line' do
-      system 'ruby -r ./khm_solution.rb -e KHMSolution.hi > tmp/output.txt'
-      output = File.open('tmp/output.txt').read
-      expect(output).to eql "Hello world!\n"
+      system "ruby -r ./khm_solution.rb -e KHMSolution.hi > #{output_text_file}"
+      expect(output_to_string).to eql "Hello world!\n"
     end
   end
+
+  describe '#target_method' do
+    it 'returns the method specified in COUNT_CALLS_TO if valid' do
+      valid_method = 'String#size'
+      ENV['COUNT_CALLS_TO'] = valid_method
+      expect(KHMSolution.target_method).to eql valid_method
+    end
+
+    it 'returns an error if COUNT_CALLS_TO is not valid'
+  end
+end
+
+def output_to_string
+  File.open(output_text_file).read
 end
